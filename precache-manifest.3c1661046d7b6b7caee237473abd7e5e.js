290bb4ea4e132d9b4fcba18789d9493ed063a8f1
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2020-04-05 11:37:52 -0300

    Updates

---
+++ precache-manifest.3c1661046d7b6b7caee237473abd7e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70a8c3d5270d91a50b360803ef8b30c",
+    "url": "/covid-arg-stats/index.html"
+  },
+  {
+    "revision": "e98d99948f89ce16f00f",
+    "url": "/covid-arg-stats/static/css/main.5e91dbcd.chunk.css"
+  },
+  {
+    "revision": "18a6f35de1f9bf69a223",
+    "url": "/covid-arg-stats/static/js/2.5635293b.chunk.js"
+  },
+  {
+    "revision": "afb0821f74f74bf82dd33ebeaa778ba6",
+    "url": "/covid-arg-stats/static/js/2.5635293b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98d99948f89ce16f00f",
+    "url": "/covid-arg-stats/static/js/main.13fc0753.chunk.js"
+  },
+  {
+    "revision": "e90df8aa0d0967ef5d19",
+    "url": "/covid-arg-stats/static/js/runtime-main.c31a3102.js"
+  }
+]);
